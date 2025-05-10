1c94aa8f4924be877a6cc101c3d13b31d8b543c5
DharmikGit <flashgohilin@gmail.com>
2025-05-10 14:34:32 +0530

    index.js moved to root

---
+++ index.js
@@ -0,0 +1,21 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+
+async function run() {
+    try {
+        // Add deployment logic here (e.g., SCP/SFTP commands via node SSH libs)
+    }
+    catch (error) {
+    }
+}
+run();
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
